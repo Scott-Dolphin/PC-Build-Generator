05fbdccb595e3cd9956ff568366d942b873f18b3
Dylanht341 <dylanht341@csu.fullerton.edu>
2026-04-26 21:36:09 -0700

    pulling latest from main

--- playwright.config.js
+++ playwright.config.js
@@ -1,31 +1,3 @@
-<<<<<<< HEAD
-// playwright.config.js
-import { defineConfig, devices } from '@playwright/test';
-
-export default defineConfig({
-  testDir: './e2e',
-  fullyParallel: false,
-  retries: process.env.CI ? 2 : 0,
-  workers: 1,
-  reporter: 'html',
-  
-  use: {
-    baseURL: 'http://127.0.0.1:5173', 
-    trace: 'on-first-retry',
-  },
-  
-  projects: [
-    { name: 'chromium', use: { ...devices['Desktop Chrome'] } },
-    { name: 'firefox', use: { ...devices['Desktop Firefox'] } },
-  ],
-  
-  webServer: {
-    command: 'npm run dev -- --host 127.0.0.1', 
-    url: 'http://127.0.0.1:5173', 
-    reuseExistingServer: !process.env.CI,
-  },
-});
-=======
 import { defineConfig } from "@playwright/test";
 
 export default defineConfig({
@@ -44,4 +16,3 @@ export default defineConfig({
     reuseExistingServer: true,
   },
 });
->>>>>>> 1f5bfadc14198b97fff8b900421aefbcf56e5250
